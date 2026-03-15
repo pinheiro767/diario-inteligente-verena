17ea9a4f255ab399cd7d2ba20c5fff36803b9dfe
pinheiro767 <claudiapinheiro767@gmail.com>
2026-03-15 20:10:39 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,42 @@
-const CACHE_NAME = "verena-pwa-v1";
+const CACHE_NAME = "verena-audit-pwa-v1";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./css/styles.css",
-  "./js/app.js",
-  "./js/storage.js",
-  "./js/export.js",
-  "./js/pwa.js",
   "./manifest.json",
+  "./css/styles.css",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
+    caches.keys().then((keys) => {
+      return Promise.all(
         keys.map((key) => {
           if (key !== CACHE_NAME) {
             return caches.delete(key);
           }
         })
-      )
-    )
+      );
+    })
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
